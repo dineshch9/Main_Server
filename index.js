f1ff041defa5a75912d1123f56e2e8cf58a4bd79
dineshch9 <dineshch489@gmail.com>
2025-02-16 00:26:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const [ saveLogToDatabase ] = require('./controllers/saveLogToDatabase.js');
-const connectDB =require('./config/db.config.js');
+// const connectDB =require('./config/db.config.js');
 const binrouter = require('./routes/binroute.js');
 const homerouter = require('./routes/homeroute.js');
 console.log("hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii");
 // dotenv.config();
 // const url=process.env.dburl;
 console.log("1111111111111111111111111111111111111111111111111111111111111111111111");
+
+async function connectDB(url){
+  try {
+    // console.log(url);
+    await mongoose.connect("mongodb+srv://dinesh:dinesh@dinesh.2k4lr.mongodb.net/cars1?retryWrites=true&w=majority&appName=Dinesh"
+    );
+    console.log("connected");
+  } catch (error) {
+    console.log(error.message);
+    console.log("not connected");
+    
+  }
+  
+} 
+
 connectDB();
+
+
+
+
+
+
 console.log("11111111111111111111111111111111111");
 
 const app = express();
