2db1c0df580c23b7e27722ddf1dbdd9416da8c5e
dineshch9 <dineshch489@gmail.com>
2025-02-16 00:06:07 +0530

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -9,7 +9,7 @@ console.log("bye");
 async function connectDB(url){
     try {
         // console.log(url);
-      await mongoose.connect("mongodb+srv://dinesh:dinesh@dinesh.2k4lr.mongodb.net/bin?retryWrites=true&w=majority&appName=Dinesh"
+      await mongoose.connect("mongodb+srv://dinesh:dinesh@dinesh.2k4lr.mongodb.net/cars1?retryWrites=true&w=majority&appName=Dinesh"
       );
       console.log("connected");
     } catch (error) {
