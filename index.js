f7ab02df7fb5c7440e56f44abbd4db94616418da
dineshch9 <dineshch489@gmail.com>
2025-02-16 10:52:47 +0530

    ....

--- index.js
+++ index.js
@@ -1,53 +1,51 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors =require('cors');
 const saveLogToDatabase = require('./controllers/saveLogToDatabase.js');
 // const connectDB =require('./config/db.config.js');
 const binrouter = require('./routes/binroute.js');
 const homerouter = require('./routes/homeroute.js');
 const app = express();
 const port = process.env.PORT || 4320;
-
-
-// dotenv.config();
+dotenv.config();
+const url =  process.env.DB_URL;
+// const DB_URL=mongodb+srv://dinesh:dinesh@dinesh.2k4lr.mongodb.net/cars1?retryWrites=true&w=majority&appName=Dinesh;
 // const url=process.env.dburl;
 
+const corsOptions = {
+  origin: '*', 
+  methods: ['GET', 'POST', 'DELETE'],  
+  allowedHeaders: ['Content-Type', 'Authorization'] 
+};
 
+app.use(cors(corsOptions));
 
-async function connectDB(){
-
-   try {
-          // console.log(url);
-        await mongoose.connect("mongodb+srv://sd:sd@dinesh.2k4lr.mongodb.net/?retryWrites=true&w=majority&appName=Dinesh"
-        );
-        console.log("connectedsu");
-      } catch (error) {
-          console.log(error);
-        console.log("not connected111",error);
-        
-      }
-
-
-
-
-
+async function connect(DB_URL){
+  try {
+    await mongoose.connect(url);
+    console.log("connected");
+  } catch (e) {
+    console.log("not connected");
+    console.log(e.message);
+    
+  }
   
 } 
-
-connectDB();
-
-
+console.log(url);
+connect(url);
 
 
 
 
-console.log("11111111111111111111111111111111111");
+
+
 
 
 // Logging middleware using 'finish' event
 app.use((req, res, next) => {
   res.on('finish', () => {
-    connectDB();
+   
     saveLogToDatabase(req, res);
   });
   next();
